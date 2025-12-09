41076b4eab115df14fae4f674a3437ca29241939
cxjin994 <2114378416@qq.com>
2025-12-09 17:44:47 +0800

    增加基本的插入打印函数

--- b循环链表.c
+++ b循环链表.c
@@ -15,17 +15,67 @@ Node* creatNode(int data){
 }
 
 //在头部插入节点
-Node* inserthead(Node* head, int data){
+Node* insertHead(Node* head, int data){
     Node* newNode=creatNode(data);
+    if(head == NULL) return newNode;
     newNode->next=head; head->last=newNode;
     return newNode;
 }
 
 //在尾部插入节点
+Node* insertTail(Node* head, int data){
+    Node* newNode=creatNode(data);
+    if(head == NULL) return newNode;
+    Node* current=head;
+    while(current->next != NULL){
+    	current=current->next;
+    }
+    current->next=newNode; newNode->last=current;
+    return head;
+}
 
+//打印链表
+void printList(Node* head){
+    if(head ==NULL){
+        printf("无链表");
+    }else{
+        Node* current=head;
+    Node* temp=NULL;
+    int i=1;
+    while(temp != head || current->next != NULL){
+    	if(head == NULL) break;
+	printf("第%d个节点是%d ", i, current->data);
+	if(current->next == NULL) break;
+	current=current->next;
+	if(temp == head) break;
+	temp=current;
+	i++;
+    }
+    }
+    
+    printf("\n");
+}
 
 int main(){
-
-
+    int ch, data;
+    Node* head=NULL;
+    printf("在头部插入节点选择：1\n");
+    printf("在尾部插入节点选择：2\n");
+    
+    printf("退出选择：0\n");
+    scanf("%d", &ch);
+    
+    while(ch!=0){
+    	switch(ch){
+	    case 1: printf("输入想插入的数"); scanf("%d", &data); head=insertHead(head,data); break;
+	    case 2: printf("输入想插入的数"); scanf("%d", &data); head=insertTail(head,data); break;
+	    case 0: break;
+	    default: printf("Error\n"); break;
+	}
+	printf("继续选择");
+	scanf("%d", &ch);
+    }
+    
+    printList(head);
     return 0;
 }
