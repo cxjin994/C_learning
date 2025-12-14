9c525f2323eb7bb1b36f3b0cc7fdc2bf480ad56c
cxjin994 <2114378416@qq.com>
2025-12-14 19:14:25 +0800

    完整函数

--- k快速排序.c
+++ k快速排序.c
@@ -15,4 +15,15 @@ int partition(int arr[], int low, int high){
 	    swap(&arr[i], &arr[k]);
 	}
     }
+    swap(&arr[i+1], &arr[high]);
+    return i+1;
+}
+//快速排序
+void quickSort(int arr[], int low, int high){
+    if(low<high){
+        int pi=partition(arr, low, high);
+
+	quickSort(arr, low, pi-1);
+	quickSort(arr, pi+1, high);
+    }
 }
