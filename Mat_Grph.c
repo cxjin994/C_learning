aa69cb292113c6dd6ffe2b8145228ece10ef231c
cxjin994 <2114378416@qq.com>
2026-05-22 21:51:16 +0800

    增加了DFS函数

--- Mat_Grph.c
+++ Mat_Grph.c
@@ -1,54 +1,75 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct{
-    int** arc;
-    int directed;  //1:有向, 0:无向
-    int vertex_num; //顶点个数
-    int edge_num;  //边个数
-}Mat_Grph;
+typedef struct
+{
+    int **arc;
+    int directed;   // 1:有向, 0:无向
+    int vertex_num; // 顶点个数
+    int edge_num;   // 边个数
+} Mat_Grph;
 
-void initGraph(Mat_Grph* G){
+void initGraph(Mat_Grph *G)
+{
     printf("有向图/无向图？ 1/0");
-    scanf("%d",&G->directed);
+    scanf("%d", &G->directed);
     printf("输入顶点个数 vertex_num=");
-    scanf("%d",&G->vertex_num);
-    
-    G->arc=(int**)malloc(G->vertex_num*sizeof(int*));
+    scanf("%d", &G->vertex_num);
 
-    for(int i=0; i < G->vertex_num; i++){
-    	(G->arc)[i]=(int*)calloc(G->vertex_num,sizeof(int)); //calloc(个数,大小)   分配空间并初始化为0
+    G->arc = (int **)malloc(G->vertex_num * sizeof(int *));
+
+    for (int i = 0; i < G->vertex_num; i++)
+    {
+        (G->arc)[i] = (int *)calloc(G->vertex_num, sizeof(int)); // calloc(个数,大小)   分配空间并初始化为0
     }
 
-    int num=0;
-    while(1){
-	int a, b;
-    	printf("输入想连接的顶点(输入 -1 退出)\n");
-	scanf("%d %d",&a,&b);
-	if(a == -1 || b == -1) break;
-	printf("输入该边的权值 若无权则赋值1\n");
-	int temp=G->arc[a][b];
-	scanf("%d",&G->arc[a][b]);
-	if(G->directed == 0){
-	    G->arc[b][a]=G->arc[a][b];
-	}
-	
-	if(temp == 0) num++;
-	
+    int num = 0;
+    while (1)
+    {
+        int a, b;
+        printf("输入想连接的顶点(输入 -1 退出)\n");
+        scanf("%d %d", &a, &b);
+        if (a == -1 || b == -1) break;
+        printf("输入该边的权值 若无权则赋值1\n");
+        int temp = G->arc[a - 1][b - 1];
+        scanf("%d", &G->arc[a - 1][b - 1]);
+        if (G->directed == 0)
+        {
+            G->arc[b - 1][a - 1] = G->arc[a - 1][b - 1];
+        }
+
+        if (temp == 0)
+            num++;
     }
 
-    G->edge_num=num;
+    G->edge_num = num;
 }
 
-int main(){
-    Mat_Grph* MG=(Mat_Grph*)malloc(sizeof(Mat_Grph));
-    initGraph(MG);
-    for(int i=0; i< MG->vertex_num; i++){
-    	for(int k=0; k< MG->vertex_num; k++){
-	    printf("%d ",(MG->arc)[i][k]);
-	}
-	printf("\n");
+void DFS(Mat_Grph *G, int index, int *visited)
+{
+    if (index < 0 || index > G->vertex_num - 1)
+        index = 0;
+
+    if (visited[index] == 0)
+        visited[index]++;
+    else
+        return;
+
+    printf("当前是第%d个顶点\n", index + 1);
+    for (int i = 0; i < G->vertex_num; i++)
+    {
+        if (G->arc[index][i] > 0)
+            DFS(G, i, visited);
     }
+}
+
+int main()
+{
+    Mat_Grph *MG = (Mat_Grph *)malloc(sizeof(Mat_Grph));
+    initGraph(MG);
+    int *visited = (int *)calloc(MG->vertex_num, sizeof(int));
+    DFS(MG, 0, visited);
     free(MG);
+    free(visited);
     return 0;
 }
